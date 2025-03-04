f1a5308cb00ab0a873aef220cdb20ee38a229bd2
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-04 18:12:29 -0300

    feat: Adiciona rotas no app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express"
 import "express-async-errors"
+import { errorHandling } from "@/middlewares/erro-handling"
+import { routes } from "@/routes"
 
 const app = express()
 app.use(express.json())
+
+app.use(routes)
+
+app.use(errorHandling)
 
 export { app }
