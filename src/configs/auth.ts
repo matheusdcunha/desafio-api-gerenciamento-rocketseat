ba3f67ed60b7add9f3e63cb5a61486d6c76dbebc
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-04 18:12:46 -0300

    feat: Cria o arquivo de configuração do JWT

---
+++ src/configs/auth.ts
@@ -0,0 +1,10 @@
+import { env } from "@/env"
+import type { StringValue } from "ms"
+
+
+export const authConfig = {
+  jwt: {
+    secret: env.JWT_SECRET,
+    expiresIn: "1d" as unknown as StringValue
+  }
+}
