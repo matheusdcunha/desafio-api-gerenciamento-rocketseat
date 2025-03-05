fd4fd1544713ec7a4be5eaa1b5e5fc57f7aa65c5
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-04 21:57:59 -0300

    fix: Nome dos arquivos teams

--- src/routes/team-routes.ts
+++ src/routes/team-routes.ts
@@ -1,32 +1,32 @@
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
-import { TeamController } from "@/controllers/team-controller";
+import { TeamController } from "@/controllers/teams-controller";
 import { Router } from "express";
 
 
-const teamRoute = Router();
+const teamRoutes = Router();
 const teamController = new TeamController();
 
-teamRoute.post("/",
+teamRoutes.post("/",
   ensureAuthenticated,
   verifyUserAuthorization(["admin"]),
   teamController.create
 )
 
-teamRoute.put("/:id",
+teamRoutes.put("/:id",
   ensureAuthenticated,
   verifyUserAuthorization(["admin"]),
   teamController.update
 )
 
-teamRoute.get("/",
+teamRoutes.get("/",
   teamController.index
 )
 
-teamRoute.delete("/:id",
+teamRoutes.delete("/:id",
   ensureAuthenticated,
   verifyUserAuthorization(["admin"]),
   teamController.delete
 )
 
-export { teamRoute }
+export { teamRoutes }
